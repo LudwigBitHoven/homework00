1277a61c3a145de6bebecc2278748f131845bf03
Николай Чаптыков <90330587+LudwigBitHoven@users.noreply.github.com>
2021-10-31 13:44:24 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,11 +1,14 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
-    res = ''
+    res = ""
     key = keyword.upper()
     for i in range(len(plaintext)):
         if plaintext[i].isalpha():
-            if (ord(plaintext[i]) + ord(key[i % len(key)]) > 187 and plaintext[i].islower() or 
-                ord(plaintext[i]) + ord(key[i % len(key)]) > 155 and plaintext[i].isupper()):
-                res += chr(ord(plaintext[i]) + ord(key[i % len(key)]) - ord("plaintext") - 26)
+            if (ord(plaintext[i]) + ord(key[i % len(key)]) > 187
+                and plaintext[i].islower()
+                or ord(plaintext[i]) + ord(key[i % len(key)]) > 155
+                and plaintext[i].isupper()
+            ):
+                res += chr(ord(plaintext[i]) + ord(key[i % len(key)]) - ord("a") - 26)
             else:
                 res += chr(ord(plaintext[i]) + ord(key[i % len(key)]) - ord("A"))
         else:
@@ -14,13 +17,16 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    res = ''
+    res = ""
     a = ciphertext
     key = keyword.upper()
     for i in range(len(a)):
         if a[i].isalpha():
-            if (ord(a[i]) - ord(key[i % len(key)]) < 32 and a[i].islower() or 
-                ord(a[i]) - ord(key[i % len(key)]) < 0 and a[i].isupper()):
+            if (ord(a[i]) - ord(key[i % len(key)]) < 32
+                and a[i].islower()
+                or ord(a[i]) - ord(key[i % len(key)]) < 0
+                and a[i].isupper()
+            ):
                 res += chr(ord(a[i]) - ord(key[i % len(key)]) + 65 + 26)
             else:
                 res += chr(ord(a[i]) - ord(key[i % len(key)]) + 65)
