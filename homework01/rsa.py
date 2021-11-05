7cd2542e0ed58a1686674d492ceceb6c6adc1e71
Николай Чаптыков <90330587+LudwigBitHoven@users.noreply.github.com>
2021-11-06 00:57:39 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -3,45 +3,55 @@ import typing as tp
 
 
 def is_prime(n: int) -> bool:
-    d = 2
-    if n == 1:
-        return False
-    while n % d != 0 and d * d <= n:
-        d += 1
-    if d * d > n:
-        return True
-    else:
-        return False
+    """
+    Tests to see if a number is prime.
+
+    >>> is_prime(2)
+    True
+    >>> is_prime(11)
+    True
+    >>> is_prime(8)
+    False
+    """
+    # PUT YOUR CODE HERE
+    pass
 
 
 def gcd(a: int, b: int) -> int:
-    while a != 0 and b != 0:
-        if a > b:
-            a = a % b
-        else:
-            b = b % a
-    return a + b
+    """
+    Euclid's algorithm for determining the greatest common divisor.
+
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
+    # PUT YOUR CODE HERE
+    pass
 
 
-def multiplicative_inverse(e: int, phi: int):
-    def xy_finder(te: int, tphi: int):
-        if te == 0:
-            return (0, 1)
-        else:
-            y, x = xy_finder(tphi % te, te)
-            return (x - (tphi // te) * y, y)
+def multiplicative_inverse(e: int, phi: int) -> int:
+    """
+    Euclid's extended algorithm for finding the multiplicative
+    inverse of two numbers.
 
-    return xy_finder(e, phi)[0] % phi
+    >>> multiplicative_inverse(7, 40)
+    23
+    """
+    # PUT YOUR CODE HERE
+    pass
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
-        raise ValueError("Both numbers must be prime")
+        raise ValueError("Both numbers must be prime.")
     elif p == q:
         raise ValueError("p and q cannot be equal")
-    n = p * q
+
+    # n = pq
     # PUT YOUR CODE HERE
-    phi = (p - 1) * (q - 1)
+
+    # phi = (p-1)(q-1)
     # PUT YOUR CODE HERE
 
     # Choose an integer e such that e and phi(n) are coprime
