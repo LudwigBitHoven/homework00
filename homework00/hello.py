8c1831e1e663aad6ac2f450aa7b3ec0d5fc693c1
Николай Чаптыков <90330587+LudwigBitHoven@users.noreply.github.com>
2021-11-13 15:03:03 +0300

    Update hello.py

--- homework00/hello.py
+++ homework00/hello.py
@@ -3,5 +3,5 @@ def get_greeting(name: str) -> str:
     return "Hello, " + name + "!"
 
 if __name__ == "__main__":
-    message = get_greeting("World")
-    print(message)
+    MESSAGE = get_greeting("World")
+    print(MESSAGE)
